5cfe25aaa35829f5c4c5a074f67f5da3854038ae
marlotina <inaki.arrieta@gmail.com>
2020-05-03 18:56:05 +0200

    no message

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: 'login', loadChildren: () => import('./account/login/login.module').then(m => m.LoginModule) },
+  { path: 'forgotpassword', loadChildren: () => import('./account/forgotpassword/forgotpassword.module').then(m => m.ForgotpasswordModule) },
+  { path: 'signup', loadChildren: () => import('./account/signup/signup.module').then(m => m.SignupModule) }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, NavComponent, AsideComponent, FooterComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [HeaderComponent, NavComponent, AsideComponent, FooterComponent]
+})
+export class LayoutModule { }
