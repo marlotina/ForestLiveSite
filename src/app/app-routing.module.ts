a95da3777f8018eb1c4577dab308f0c361ebfa7d
marlotina <inaki.arrieta@gmail.com>
2020-05-04 01:16:29 +0200

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ const routes: Routes = [
   { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: 'login', loadChildren: () => import('./account/login/login.module').then(m => m.LoginModule) },
   { path: 'forgotpassword', loadChildren: () => import('./account/forgotpassword/forgotpassword.module').then(m => m.ForgotpasswordModule) },
-  { path: 'signup', loadChildren: () => import('./account/signup/signup.module').then(m => m.SignupModule) }];
+  { path: 'signup', loadChildren: () => import('./account/signup/signup.module').then(m => m.SignupModule) },
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
