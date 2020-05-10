6f6f365cfad5f26dd4970f25d665ea4857b79e50
marlotina <inaki.arrieta@gmail.com>
2020-05-10 20:02:40 +0200

    top header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from  '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
@@ -7,22 +6,8 @@ import { TranslateService } from  '@ngx-translate/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(  
-    public translate: TranslateService) {  
-    translate.addLangs(['en', 'es', 'de']);  
-
-    if (localStorage.getItem('locale')) {  
-      const browserLang = localStorage.getItem('locale');  
-      translate.use(browserLang.match(/en|de|es/) ? browserLang : 'en');  
-    } else {  
-      localStorage.setItem('locale', 'en');  
-      translate.setDefaultLang('en');  
-    }  
-  }  
-
-  changeLang(language: string) {  
-    localStorage.setItem('locale', language);  
-    this.translate.use(language);  
+  constructor() {  
+   
   }  
 
   ngOnInit(): void {
