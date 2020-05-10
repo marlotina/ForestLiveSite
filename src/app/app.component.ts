17e5e34342a0157d62fd9d97fa3419fa6d5ccc72
marlotina <inaki.arrieta@gmail.com>
2020-05-10 19:46:30 +0200

    javascript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
 
   title = 'fl-site';
+  ngOnInit() {
+    this.loadScript('../assets/js/active.js');
+  }
+
+ public loadScript(url: string) {
+   const body = <HTMLDivElement> document.body;
+   const script = document.createElement('script');
+   script.innerHTML = '';
+   script.src = url;
+   script.async = false;
+   script.defer = true;
+   body.appendChild(script);
+ }
 }
+
+
+
+
 
 
 
