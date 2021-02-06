cace0c0ea5590fb0177e3c999c04911467d6d1e5
marlotina <inaki.arrieta@gmail.com>
2021-02-06 21:15:25 +0100

    add remove angular material modules

--- site/src/app/components/postpage/post-page/post-page.module.ts
+++ site/src/app/components/postpage/post-page/post-page.module.ts
@@ -7,28 +7,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 
-import { MatDatepickerModule } from '@angular/material/datepicker';
-
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
-import {MatSelectModule} from '@angular/material/select';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 const materialModules = [
   MatMenuModule, 
-  MatDatepickerModule,
-  MatNativeDateModule,
-  MatFormFieldModule,
   MatButtonModule,
-  MatInputModule,
-  MatChipsModule,
   MatIconModule,
-  MatSelectModule,
-  MatAutocompleteModule
 ];
 
 
