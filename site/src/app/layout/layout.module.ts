879a198818eb1c01a8fc40abc0f4fa3b9f018a57
marlotina <inaki.arrieta@gmail.com>
2021-03-15 23:52:00 +0100

    top header remove

--- site/src/app/layout/layout.module.ts
+++ site/src/app/layout/layout.module.ts
@@ -2,18 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { TopHeaderComponent } from './top-header/top-header.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, TopHeaderComponent],
+  declarations: [HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
     SharedModule,
     RouterModule
   ],
-  exports: [HeaderComponent, FooterComponent, TopHeaderComponent]
+  exports: [HeaderComponent, FooterComponent]
 })
 export class LayoutModule { }
