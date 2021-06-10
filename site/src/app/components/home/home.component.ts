8f87b85855fa6691d37ec273354ed7f9c256467f
marlotina <inaki.arrieta@gmail.com>
2021-06-10 11:38:40 +0200

    remove code without use

--- site/src/app/components/home/home.component.ts
+++ site/src/app/components/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PostHomeResponse } from 'src/app/model/post';
-import { AccountService } from 'src/app/services/account/account.service';
 import { BirdserviceService } from 'src/app/services/bird/birdservice.service';
 import { environment } from 'src/environments/environment';
 
@@ -16,23 +15,10 @@ export class HomeComponent implements OnInit {
   urlPostImage: string = environment.imagesPostUrl;
 
   constructor(
-    private accountService: AccountService,
     private birdService: BirdserviceService
   ) { }
 
   ngOnInit(): void {
-    if(this.accountService.user){
-      
-      this.accountService.user.subscribe(
-        x => {
-          if(x != null)
-          {
-            this.userPageUrl = `userpage/${x.userId}`;
-          }
-        }
-      );
-    }
-
     this.birdService.GetLastbirds().subscribe(
       data => {
         this.lastObservations = data;
